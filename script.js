aa1579690b6101697155429dd288cc2b308da9bd
PiotrAntolik <antolcrew@gmail.com>
2022-06-19 14:10:55 +0200

    remove card content instead added swiper API plugin and main-content category section

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("siema");
+const swiper = new Swiper('.swiper', {
+    
+
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      
+
+  });
